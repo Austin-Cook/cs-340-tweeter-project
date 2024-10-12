3cf47fbb17c6233f52d2594b7c7dacec5865358f
Austin Cook <austin.cook.id@gmail.com>
2024-10-12 13:37:17 -0600

    Test Login Component

--- tweeter-web/src/components/authentication/AuthenticationFields.tsx
+++ tweeter-web/src/components/authentication/AuthenticationFields.tsx
@@ -19,6 +19,7 @@ const AuthenticationFields = (props: Props) => {
           className="form-control"
           size={50}
           id="aliasInput"
+          aria-label="alias"
           placeholder="name@example.com"
           onKeyDown={(event) => presenter.onEnter(event, props.onEnter,
             props.checkSubmitButtonStatus)}
@@ -31,6 +32,7 @@ const AuthenticationFields = (props: Props) => {
           type="password"
           className="form-control bottom"
           id="passwordInput"
+          aria-label="password"
           placeholder="Password"
           onKeyDown={(event) => presenter.onEnter(event, props.onEnter,
             props.checkSubmitButtonStatus)}
