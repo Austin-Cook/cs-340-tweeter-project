fc689340b0559d91e963870b8cef1f13b7810639
Austin Cook <austin.cook.id@gmail.com>
2024-11-07 11:54:03 -0700

    Create server services

---
+++ tweeter-server/src/model/service/UserService.ts
@@ -0,0 +1,112 @@
+import { Buffer } from "buffer";
+import { AuthTokenDto, FakeData, UserDto } from "tweeter-shared";
+
+export class UserService {
+  public async getUser(
+    token: string,
+    alias: string
+  ): Promise<UserDto | null> {
+    const user = FakeData.instance.findUserByAlias(alias);
+    const userDto: UserDto | null = user == null ? null : user.dto;
+
+    return userDto;
+  };
+
+  public async login(
+    alias: string,
+    password: string
+  ): Promise<[UserDto, AuthTokenDto]> {
+    const user = FakeData.instance.firstUser;
+
+    if (user === null) {
+      throw new Error("Invalid alias or password");
+    }
+
+    return [user.dto, FakeData.instance.authToken.dto];
+  };
+
+  public async logout(token: string): Promise<void> {
+    // Pause so we can see the logging out message. Delete when the call to the server is implemented.
+    await new Promise((res) => setTimeout(res, 1000));
+
+    // TODO: Log the user out
+  };
+
+  public async register(
+    firstName: string,
+    lastName: string,
+    alias: string,
+    password: string,
+    userImageBase64: string,
+    imageFileExtension: string
+  ): Promise<[UserDto, AuthTokenDto]> {
+    // // Originally: userImageBytes: Uint8Array,
+    // // Not neded now, but will be needed when you make the request to the server in milestone 3
+    // const imageStringBase64: string =
+    //   Buffer.from(userImageBytes).toString("base64");
+
+    // TODO: Replace with the result of calling the server
+    const user = FakeData.instance.firstUser;
+
+    if (user === null) {
+      throw new Error("Invalid registration");
+    }
+
+    return [user.dto, FakeData.instance.authToken.dto];
+  };
+
+  public async getIsFollowerStatus(
+    token: string,
+    user: UserDto,
+    selectedUser: UserDto
+  ): Promise<boolean> {
+    // TODO: Replace with the result of calling server
+    return FakeData.instance.isFollower();
+  };
+
+  public async getFolloweeCount(
+    token: string,
+    user: UserDto
+  ): Promise<number> {
+    // TODO: Replace with the result of calling server
+    return FakeData.instance.getFolloweeCount(user.alias);
+  };
+
+  public async getFollowerCount(
+    token: string,
+    user: UserDto
+  ): Promise<number> {
+    // TODO: Replace with the result of calling server
+    return FakeData.instance.getFollowerCount(user.alias);
+  };
+
+  public async follow(
+    token: string,
+    userToFollow: UserDto
+  ): Promise<[followerCount: number, followeeCount: number]> {
+    // Pause so we can see the follow message. Remove when connected to the server
+    await new Promise((f) => setTimeout(f, 2000));
+
+    // TODO: Call the server
+
+    const followerCount = await this.getFollowerCount(token, userToFollow);
+    const followeeCount = await this.getFolloweeCount(token, userToFollow);
+
+    return [followerCount, followeeCount];
+  };
+
+  public async unfollow(
+    token: string,
+    userToUnfollow: UserDto
+  ): Promise<[followerCount: number, followeeCount: number]> {
+    // Pause so we can see the unfollow message. Remove when connected to the server
+    await new Promise((f) => setTimeout(f, 2000));
+
+    // TODO: Call the server
+
+    const followerCount = await this.getFollowerCount(token, userToUnfollow);
+    const followeeCount = await this.getFolloweeCount(token, userToUnfollow);
+
+    return [followerCount, followeeCount];
+  };
+}
