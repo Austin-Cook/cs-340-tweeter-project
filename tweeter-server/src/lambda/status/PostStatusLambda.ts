34b8850071b2fb2dfe57df971fb35f1ca8ae906a
Austin Cook <austin.cook.id@gmail.com>
2024-11-07 11:55:41 -0700

    Create lambda functions

---
+++ tweeter-server/src/lambda/status/PostStatusLambda.ts
@@ -0,0 +1,22 @@
+import { PostStatusRequest } from "tweeter-shared"
+import { StatusService } from "../../model/service/StatusService";
+import { TweeterResponse } from "tweeter-shared/dist/model/net/response/TweeterResponse";
+import { validateStatus, validRequest } from "../util/ValidateInput";
+import { getMissingRequestFieldResponse, getMissingStatusFieldResponse } from "../util/Error";
+
+export const handler = async (request: PostStatusRequest): Promise<TweeterResponse> => {
+  if (!validRequest(request.token, request.newStatus)) {
+    return getMissingRequestFieldResponse<TweeterResponse>();
+  }
+  if (!validateStatus(request.newStatus)) {
+    return getMissingStatusFieldResponse<TweeterResponse>();
+  }
+
+  const statusService = new StatusService();
+  await statusService.postStatus(request.token, request.newStatus);
+
+  return {
+    success: true,
+    message: null
+  }
+}
